0b0f19e244682dfccc5c5541c31c562cbca329f9
Barış <barisgungor@Barss-Mac.local>
2019-04-22 05:32:20 -0700

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Barış Güngör on 22.04.2019.
+//  Copyright © 2019 Baris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
